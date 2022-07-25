805afa136aafd62179ccffdee3e80e6b4dc62381
danar <danarvare2003@gmail.com>
2022-07-24 18:52:18 -0600

    Simplifico todo el cdigo original

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,55 +12,15 @@ fun main() {
 }
 
 fun processList(inputList: List<Any?>?): List<ItemData>? {
-    if(inputList == null){
-        return null
-    }
-
     val result= ArrayList<ItemData>()
-    val count:Int= inputList!!.size -1
-    var str:String?
-    var bool:Boolean?
-    var entero:Int?
-    var type:String? = ""
-    var info:String? =""
-
-    for(i in 0..count){
-
-        when(inputList[i]){
-            is Int ->
-            {type="entero"
-
-                entero= inputList[i] as? Int
-                if((entero ?: 2)%10 == 0){
-                    info= "M10"
-                } else if((entero ?: 3)%5==0){
-                    info="M5"
-                }else if((entero ?: 3)%2==0){
-                    info="M2"
-                }else {
-                    info=null
-                }
-
-            }
-            is Boolean -> {
-                type ="booleano"
-                bool = inputList[i] as? Boolean
-                info = if((bool ?: true)) "verdadero" else "falso"
-
-            }
-            is String -> {
-                type ="cadena"
-                str = inputList[i] as? String
-                info= "L"+ (str ?: "").length
-            }
-            else -> {type =null
-                info=null
-            }
+    inputList?.let {
+        for(item in inputList){
+                val itemData: ItemData = when(item){
+                    is Int -> ItemData(inputList.indexOf(item), item,"entero", if(item%10 == 0){ "M10"} else if(item%5==0){ "M5" } else if(item %2==0){ "M2" } else { null})
+                    is Boolean ->ItemData(inputList.indexOf(item),item, "booleano", if(item != false) "verdadero" else "falso" )
+                    is String ->  ItemData(inputList.indexOf(item), item, "cadena", "L"+item.length)
+                    else -> ItemData(inputList.indexOf(item), item?: "po") }
+                item?.let{result.add(itemData)}
         }
-        if(inputList[i]!= null){
-            result.add(ItemData(i, (inputList[i] ?: "null"),type,info ))
-        }
-
-    }
-    return result
-}
+        return result
+    }?: return null}
